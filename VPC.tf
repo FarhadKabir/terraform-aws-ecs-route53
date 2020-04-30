622d364888004c8e047eeedc851fb68a5755c2e6
FarhadKabir <farhad_kabir1@yahoo.com>
2020-04-30 06:19:36 -0700

    add internet gateway and route for it in route table

--- VPC.tf
+++ VPC.tf
@@ -28,3 +28,17 @@ resource "aws_subnet" "public2" {
     Name = "Public-SN2"
   }
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "IGW"
+  }
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.main.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
