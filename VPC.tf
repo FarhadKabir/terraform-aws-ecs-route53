b0dd422e98c2510612951c25fa52520bf2bd8c3a
FarhadKabir <farhad_kabir1@yahoo.com>
2020-05-04 05:06:17 -0700

    add security groups for alb and ECS

--- VPC.tf
+++ VPC.tf
@@ -70,3 +70,24 @@ resource "aws_security_group" "lb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# Traffic to the ECS Cluster should only come from the ALB
+resource "aws_security_group" "ecs_tasks" {
+  name        = "SG-ECS"
+  description = "allow inbound access from the ALB only"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = "80"
+    to_port         = "80"
+    security_groups = [aws_security_group.lb.id]
+  }
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
